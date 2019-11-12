17104aab4c15dac565a4efb5c030349f9cf43831
strangehood <55440847+strangehood@users.noreply.github.com>
2019-11-12 15:04:45 +0300

    fix everything

--- solar_model.py
+++ solar_model.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # license: GPLv3
 
-gravitational_constant = 6.67408E-11
+G = gravitational_constant = 6.6743E-11
 """Гравитационная постоянная Ньютона G"""
 
 
@@ -16,11 +16,10 @@ def calculate_force(body, space_objects):
 
     body.Fx = body.Fy = 0
     for obj in space_objects:
-        if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  # FIXME: нужно вывести формулу...
-        body.Fy += 2  # FIXME: нужно вывести формулу...
+        if body != obj:
+            r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+            body.Fx += G * body.m * obj.m * (- body.x + obj.x) / r ** 3  # FIXME: нужно вывести формулу...
+            body.Fy += G * body.m * obj.m * (- body.y + obj.y) / r ** 3  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
@@ -32,8 +31,13 @@ def move_space_object(body, dt):
     """
 
     ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
-    body.Vx += ax*dt
+    body.Vx += ax * dt
+    body.x += body.Vx * dt  # FIXME: не понимаю как менять...
+
+    ay = body.Fy / body.m
+    body.Vy += ay * dt
+    body.y += body.Vy * dt
+
     # FIXME: not done recalculation of y coordinate!
 
 
